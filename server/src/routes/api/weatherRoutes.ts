165fc40d39471676ba0a5388a033dbbf023ff173
Nic Nic Miller <nicmiller13@gmail.com>
2024-10-16 19:51:37 -0400

    worked on router.get in weather routes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,17 +1,33 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+ import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 // test these in insomnia!!
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+  const cityWeather = await WeatherService.getWeatherByCity(req.body.city);
+  res.json(cityWeather);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
   // TODO: GET weather data from city name
   // TODO: save city to search history
+  //do I need to make a seperate function for this or can I put it in the same try block?
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (req, res) => {
+  try {
+    const savedCities = await HistoryService.getCities(req.body.city);
+    res.json(savedCities);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req, res) => {});
